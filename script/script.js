065c6a374933313b9c2fb79f3fbb28d56ef087cc
ManuRP99 <155848017+ManuRP99@users.noreply.github.com>
2024-01-29 18:31:45 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const rows = 20;
+const columns = 20;
+const arrayDiv = document.querySelector('#array');
+const genArrayBtn = document.getElementById('generate-array');
+let hiddenText = document.querySelectorAll('.hidden');
+
+
+
+
+function genArray() {
+   
+    for (let y = 0; y < rows; y++){
+        for (let x = 0; x < columns; x++) {
+            let pixelId = y + '-' + x;
+            let dArray = document.createElement('div');
+            dArray.className = 'pixel';
+            dArray.setAttribute('id', pixelId);
+            arrayDiv.appendChild(dArray);
+        }
+    }
+    
+genArrayBtn.className += 'hidden';
+
+}
+
