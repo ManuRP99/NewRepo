a9613b7d2482467192940dbb73a4cbdff7480776
ManuRP99 <155848017+ManuRP99@users.noreply.github.com>
2024-01-30 01:54:24 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,24 +2,47 @@ const rows = 20;
 const columns = 20;
 const arrayDiv = document.querySelector('#array');
 const genArrayBtn = document.getElementById('generate-array');
+const randomPixelBtn = document.getElementById('random-pixel');
 
 
+function genArray(c, r) {
+    let array = new Array(c);
+     for (let i = 0; i < r; i++) {
+        array[i] = new Array(c);
+    }
+return array;
+}
 
+const emptyArray = genArray(columns, rows);
+console.table(emptyArray);
 
-
-function genArray() {
-   
-    for (let y = 0; y < rows; y++){
-        for (let x = 0; x < columns; x++) {
-            let pixelId = y + '-' + x;
-            let dArray = document.createElement('div');
-            dArray.className = 'pixel';
-            dArray.setAttribute('id', pixelId);
-            arrayDiv.appendChild(dArray);
+function showArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++){
+            let emptyCell = document.createElement('div');
+            let pixelId = i + ' ' + j;
+            emptyCell.className = 'pixel';
+            emptyCell.setAttribute('id', pixelId);
+            arrayDiv.appendChild(emptyCell);
         }
     }
-    
-genArrayBtn.className += 'hidden';
-
-
+    genArrayBtn.className += 'hidden';
+    randomPixelBtn.classList.remove('hidden');
 }
+
+
+
+function aliveCell(col, row) {
+    let target = document.getElementById(col + ' ' + row);
+    target.classList.add('alive');
+}
+
+function showRandomCell() {
+    let ra = Math.floor(Math.random() *20);
+    let rb = Math.floor(Math.random() *20);
+    aliveCell(ra, rb);
+    
+};
+
+//works until here
+
