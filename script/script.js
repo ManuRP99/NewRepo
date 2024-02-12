b151e55fb893899851a62e4d768e906f6f0b0279
ManuRP99 <manuelrampat@gmail.com>
2024-02-12 21:38:14 +0100

    aesthetic improvement

--- script/script.js
+++ script/script.js
@@ -2,12 +2,24 @@ const barHours = document.getElementById('arrHours');
 const barMinutes = document.getElementById('arrMinutes');
 const barSeconds = document.getElementById('arrSeconds');
 
+
+const barHours1 = document.getElementById('arrHours1');
+const barMinutes1 = document.getElementById('arrMinutes1');
+const barSeconds1 = document.getElementById('arrSeconds1');
+
+
+
+
+
+
+
 function fillWithT(place, number) {
-	place.innerText = '';
+	place.innerText = '~';
 	for (let i = 0; i < number; i++) {
 		place.innerText += '#';
 //		console.log('test');
 	}
+	place.innerText += '~';
 }
 
 
@@ -18,8 +30,11 @@ function setClock() {
 	const minutes = currentDate.getMinutes();
 	const seconds = currentDate.getSeconds();
 	fillWithT(barHours, hours);
+	fillWithT(barHours1, hours);
 	fillWithT(barMinutes, minutes);
+	fillWithT(barMinutes1, minutes);
 	fillWithT(barSeconds, seconds);
+	fillWithT(barSeconds1, seconds);
 //	console.log(hours);
 //	console.log(minutes);
 //	console.log(seconds);
